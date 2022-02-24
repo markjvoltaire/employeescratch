066eb64e69b09c7bc31a7ded9f38fa12ab1bcfdb
Mark Voltaire <markjvoltaire@gmail.com>
2022-02-23 21:39:02 -0500

    userContext added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
+import { UserProvider } from './context/UserContext';
 
 export default function App() {
   return (
