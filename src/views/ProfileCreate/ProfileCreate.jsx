ccbc8f25e517cae2fa3515e1ccaf7f76420fb437
Mark Voltaire <markjvoltaire@gmail.com>
2022-02-25 19:22:20 -0500

    push for credit

---
+++ src/views/ProfileCreate/ProfileCreate.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { createProfile } from '../../services/profiles';
+
+export default function ProfileCreate() {
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      const get
+  })
+
+  return <div>ProfileCreate</div>;
+}
